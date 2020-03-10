3d7d3ae2f4e8ace2a7740800ccabe5d3d9937b5f
krzysztofsala97 <48723992+krzysztofsala97@users.noreply.github.com>
2020-03-10 22:46:41 +0100

    Start_Configuration
    After first configuring of PyCharm

---
+++ lab1/lab1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""code template"""
+
+import numpy as np
+
+
+def main():
+    P = np.array([[[0.108, 0.012], [0.072, 0.008]],
+                 [[0.016, 0.064], [0.114, 0.576]]])
+    print(P)
+
+    
+
+if __name__ == '__main__':
+    main()
