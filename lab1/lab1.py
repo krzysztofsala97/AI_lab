383a7e6dfd8deff1bc815d3bb615b829560bc276
krzysztofsala97 <48723992+krzysztofsala97@users.noreply.github.com>
2020-03-11 16:47:51 +0100

    To Ad. 4

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -7,10 +7,17 @@ import numpy as np
 
 def main():
     P = np.array([[[0.108, 0.012], [0.072, 0.008]],
-                 [[0.016, 0.064], [0.114, 0.576]]])
+                 [[0.016, 0.064], [0.144, 0.576]]])
     print(P)
 
-    
+    P_toothache = np.sum(P, axis=(0, 2))
+    print(P_toothache)
+    P_cavity = np.sum(P, axis=(1, 2))
+    print(P_cavity)
+    print("P(Toothache) wynosi " + str(P_toothache) +" a P(cavity) jest równe " + str(P_cavity))
+
+    P_toothache_cavity = np.transpose(np.sum(P, axis=2))
+    print(P_toothache_cavity)
 
 if __name__ == '__main__':
     main()
