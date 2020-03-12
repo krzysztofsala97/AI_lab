8b3fc4a80be4704cb09bc56642676d469c144044
krzysztofsala97 <48723992+krzysztofsala97@users.noreply.github.com>
2020-03-13 00:15:26 +0100

    Do_zadania_5

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -6,18 +6,21 @@ import numpy as np
 
 
 def main():
+# zadanie 1
     P = np.array([[[0.108, 0.012], [0.072, 0.008]],
                  [[0.016, 0.064], [0.144, 0.576]]])
     print(P)
-
+# oś 0 - cavity, oś 1 - toothache, oś 2 - catch
+# zadanie 2
     P_toothache = np.sum(P, axis=(0, 2))
-    print(P_toothache)
+    print("P(Toothache)= " + str(P_toothache))
+# zadanie 3
     P_cavity = np.sum(P, axis=(1, 2))
-    print(P_cavity)
-    print("P(Toothache) wynosi " + str(P_toothache) +" a P(cavity) jest równe " + str(P_cavity))
-
-    P_toothache_cavity = np.transpose(np.sum(P, axis=2))
-    print(P_toothache_cavity)
-
+    print("P(cavity) = " + str(P_cavity))
+# zadanie 4
+    P_toothache_giv_cavity = np.sum(P, axis=2)/P_cavity
+    print("P(Toothache|Cavity) = " + str(P_toothache_giv_cavity))
+# zadanie 5
+    
 if __name__ == '__main__':
     main()
