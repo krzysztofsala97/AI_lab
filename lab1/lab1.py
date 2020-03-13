c5c72da509ca572b11f4778bf468491c4185d42a
krzysztofsala97 <48723992+krzysztofsala97@users.noreply.github.com>
2020-03-13 01:04:56 +0100

    Do_zadania_8

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -21,6 +21,19 @@ def main():
     P_toothache_giv_cavity = np.sum(P, axis=2)/P_cavity
     print("P(Toothache|Cavity) = " + str(P_toothache_giv_cavity))
 # zadanie 5
-    
+    too_or_cat = np.sum(P, axis=(0, 1, 2)) - np.sum(P[:, 1, 1], axis=0)
+    print(too_or_cat)
+    P_Cav_giv_too_or_cat = (P_cavity - P[:, 1, 1])/too_or_cat
+    print("P(Cavity|toothache∨catch) = " + str(P_Cav_giv_too_or_cat))
+# zadanie 6
+    print("Wielkość tablicy wyraża się wzorem 2^ilość_zmiennych")
+# zadanie 7
+    rozmiar_b = 32 * 2 ** 32
+    rozmiar_B = rozmiar_b / 8
+    rozmiar_KB = rozmiar_B / 1024
+    rozmiar_MB = rozmiar_KB / 1024
+    rozmiar_GB = rozmiar_MB / 1024
+    print("Do przechowania 32 zmiennych binarnych zapisanych jako 32 bitowy float potrzeba " + str(rozmiar_GB) + "GB")
+# zadanie 8
 if __name__ == '__main__':
     main()
